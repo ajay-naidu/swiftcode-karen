96a3c2a7e2b2e408f25f182a43037a8ffb147c7c
ajay-naidu <iamajaynaidu@gmail.com>
2017-09-15 12:15:48 +0000

    ui

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -16,8 +16,11 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
+
+   
+
     public Result index() {
-        return ok("hello world");
+        return ok(views.html.index.render());
     }
 
 }
